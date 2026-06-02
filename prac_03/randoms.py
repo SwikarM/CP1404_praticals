20d03bdd4609f97e06b325a0332562e7c283b329
maina <mainaliswikar@gmail.com>
2026-06-02 14:57:32 +1000

    answered the questions and write code to produce random number between 1 and 100

---
+++ prac_03/randoms.py
@@ -0,0 +1,6 @@
+#1. "5 is the smallest number and 20 is the largest number i have seen"
+#2. "3 is the smallest number and 9 is the largest and it cant have produce 4 as it skip to 5 after first number."
+#3. "The result will be somewhere between 2.5 and 5.5"
+
+import random
+print(random.randint(1,100))
