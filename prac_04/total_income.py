dbcb7525192ebed9c0b7978b958e0304daa250dc
maina <mainaliswikar@gmail.com>
2026-06-16 14:50:28 +1000

    add total_income.py to calculate cumulative income over specified months

---
+++ prac_04/total_income.py
@@ -0,0 +1,14 @@
+"""
+CP1404/CP5632 Practical
+Cumulative Total Income Program
+"""
+
+
+def main():
+    """Display income report for incomes over a given number of months."""
+    incomes = []
+    number_of_months = int(input("How many months? "))
+
+    for month in range(1, number_of_months + 1):
+        income = float(input(f"Enter income for month {month}: "))
+        incomes.append(income)
