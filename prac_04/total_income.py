1168042cc3875c168e071bb795fd58e0fad627cf
maina <mainaliswikar@gmail.com>
2026-06-16 14:51:55 +1000

    add print_income_report function to display formatted income report

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -12,3 +12,14 @@ def main():
     for month in range(1, number_of_months + 1):
         income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
+        print_income_report(incomes)
+
+def print_income_report(incomes):
+        """Print a nicely formatted income report from the list of incomes."""
+        print("\nIncome Report\n-------------")
+        total = 0
+        for month, income in enumerate(incomes, start=1):
+            total += income
+            print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
+
+main()
